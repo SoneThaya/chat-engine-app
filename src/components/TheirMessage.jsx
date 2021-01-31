ceaf7571a8610f6f2d082aa45f64e424308b6bdb
Sone <sthaya@gmail.com>
2021-01-30 17:45:10 -0800

    created there message components

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,11 +1,37 @@
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
 
-
-const TheirMessage = () => {
-    return (
-        <div>
-            Their Message
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      // check if message is an image
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#cabcdc",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default TheirMessage
+export default TheirMessage;
